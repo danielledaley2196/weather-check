ece770c65cbb2297d5723ab7448160b1465a7c40
Danielle Daley <danielledaley2196@gmail.com>
2020-02-09 12:38:50 -0500

    seeing if this will fix my origin issue

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
+// ajax  call for weather
+//push to local storage
+//pull from local storage
+//get api key
 
+let apiKey = "e24495f44919a5535deac5311a5213a8"
+let placeInfo = "q=stratham,nh,us"
+
+let queryURL = "api.openweathermap.org/data/2.5/forecast?" + placeInfo + "&appid=" + apiKey;
+
+$.get(queryURL, function(response) {
+    console.log(queryURL);
+    console.log(response);
+})
 });
