cb16b3e37f2aaacf97196465c9aa0a00cf2b5bc3
Danielle Daley <danielledaley2196@gmail.com>
2020-02-10 16:51:45 -0500

    on clock for search button

--- script.js
+++ script.js
@@ -4,13 +4,22 @@ $(document).ready(function() {
 //pull from local storage
 //get api key
 
-let apiKey = "e24495f44919a5535deac5311a5213a8"
-let placeInfo = "stratham,nh,us"
-
+let apiKey = "e24495f44919a5535deac5311a5213a8";
+let placeInfo;
+let searchBtn = document.querySelector(".search-button");
 let queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + placeInfo + "&appid=" + apiKey;
-
+console.log(searchBtn);
 $.get(queryURL, function(response) {
     console.log(queryURL);
     console.log(response);
 })
+
+// get input data here
+searchBtn.on("click", function() {
+ let searchInput = $("input").val();
+ console.log(searchInput);
+})
+
+
+//save to local storage
 });
